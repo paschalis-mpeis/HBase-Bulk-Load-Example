c6941435b0e809e1ff798550724ae371411286ed
Paschalis <paschalis.mpeis@gmail.com>
2013-01-26 13:03:44 +0200

    Passing parameters example

---
+++ src/cy/ac/ucy/paschalis/hbase/bulkimport/HColumnEnum.java
@@ -0,0 +1,20 @@
+package cy.ac.ucy.paschalis.hbase.bulkimport;
+
+/**
+ * HBase table columns for the 'srv' column family
+ */
+public enum HColumnEnum {
+	SRV_COL_B("c1".getBytes()),
+	SRV_COL_C("c2"
+			.getBytes());
+
+	private final byte[] columnName;
+
+	HColumnEnum(byte[] column) {
+		this.columnName = column;
+	}
+
+	public byte[] getColumnName() {
+		return this.columnName;
+	}
+}
